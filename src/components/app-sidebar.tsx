abe8b0f13519f1331fe5f1bcfc7505f2c55e0665
Mykel68 <olowookeremicheal9568@gmail.com>
2025-05-26 14:05:37 +0100

    feat: Add hover effect to sidebar menu button for improved UI interaction

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -63,7 +63,7 @@ export function AppSidebar(props: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton
               asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5 "
+              className="data-[slot=sidebar-menu-button]:!p-1.5 hover:!bg-transparent "
             >
               <div className="flex items-center gap-2">
                 <img
