60fcdd1504dc2d4059b1b24dc816b94dfc911b73
Mplus <olowookeremicheal9568@gmail.com>
2025-07-21 18:39:01 +0100

    chore:to get daily attendacne api

---
+++ src/app/api/attendance/daily/[school_id]/[session_id]/[term_id]/[class_id]/[date]/route.ts
@@ -0,0 +1,60 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+import { cookies } from "next/headers";
+import { backendClient } from "@/lib/backendClient";
+
+export async function GET(
+  request: Request,
+  context: {
+    params: Promise<{
+      school_id: string;
+      session_id: string;
+      term_id: string;
+      class_id: string;
+      date: string;
+    }>;
+  }
+) {
+  const { school_id, session_id, term_id, class_id, date } =
+    await context.params;
+  const backendUrl = process.env.MAIN_BACKEND_URL;
+  if (!backendUrl) throw new Error("MAIN_BACKEND_URL is not set");
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get("s_id")?.value;
+
+  if (!token) {
+    return NextResponse.json(
+      { error: "Unauthorized: no token" },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const response = await backendClient.get(
+      `${backendUrl}/api/attendance/today?school_id=${school_id}&session_id=${session_id}&term_id=${term_id}&class_id=${class_id}&date=${date}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    return NextResponse.json(response.data, { status: 200 });
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      console.error("Backend error:", err.response?.data);
+      return NextResponse.json(
+        {
+          error: err.response?.data?.message || "Verification failed",
+        },
+        { status: err.response?.status || 500 }
+      );
+    }
+    console.error(err);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
